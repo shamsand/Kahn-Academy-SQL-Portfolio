d3581211cedfc789314cf6dd728ea68f7e8bf724
Shannon Sanders <97716735+shamsand@users.noreply.github.com>
2022-04-09 21:03:40 -0500

    Create FamousPeople.sql

---
+++ FamousPeople.sql
@@ -0,0 +1,46 @@
+/* Create table about the people and what they do here */
+CREATE TABLE movie_stars (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    fullname TEXT,
+    age INTEGER);
+    
+INSERT INTO movie_stars (fullname, age) VALUES ("Bobby McBobbyFace", "47");
+INSERT INTO movie_stars (fullname, age) VALUES ("Lucy BoBucie", "25");
+INSERT INTO movie_stars (fullname, age) VALUES ("Banana FoFanna", "34");
+INSERT INTO movie_stars (fullname, age) VALUES ("Shish Kabob", "20");
+INSERT INTO movie_stars (fullname, age) VALUES ("Fluffy Sparkles", "18");
+
+CREATE table movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    person_id INTEGER,
+    title TEXT);
+    
+INSERT INTO movies (person_id, title) VALUES (1, "Spider Man");
+INSERT INTO movies (person_id, title) VALUES (1, "Venom");
+INSERT INTO movies (person_id, title) VALUES (2, "Black Widow");
+INSERT INTO movies (person_id, title) VALUES (2, "Ghostbusters");
+INSERT INTO movies (person_id, title) VALUES (3, "Godzilla vs Kong");
+INSERT INTO movies (person_id, title) VALUES (3, "Mortal Kombat");
+INSERT INTO movies (person_id, title) VALUES (3, "Cruella");
+INSERT INTO movies (person_id, title) VALUES (4, "Peter Rabbit");
+INSERT INTO movies (person_id, title) VALUES (4, "Boss Baby");
+INSERT INTO movies (person_id, title) VALUES (4, "Space Jam");
+
+CREATE table married (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    person1_id INTEGER,
+    person2_id INTEGER);
+
+INSERT INTO married (person1_id, person2_id)
+    VALUES (1, 4);
+INSERT INTO married (person1_id, person2_id)
+    VALUES (2, 3);
+
+select *
+from movie_stars;
+
+select *
+from movies;
+
+select *
+from married;
