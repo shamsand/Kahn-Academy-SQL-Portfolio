a3754c183d9f7fad5734c737a37193c219ae95a8
Shannon Sanders <97716735+shamsand@users.noreply.github.com>
2022-04-10 11:29:05 -0500

    Create AppImpersonator.sql

---
+++ AppImpersonator.sql
@@ -0,0 +1,3 @@
+*/ Think about your favorite apps, and pick one that stores your data- like a game that stores scores, an app that lets you post updates, etc. 
+Now in this project, you're going to imagine that the app stores your data in a SQL database (which is pretty likely!), 
+and write SQL statements that might look like their own SQL. */
