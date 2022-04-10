0f2f0f5533d5441949a22df794b1828a186cf037
Shannon Sanders <97716735+shamsand@users.noreply.github.com>
2022-04-10 11:27:07 -0500

    Update FamousPeople.sql

--- FamousPeople.sql
+++ FamousPeople.sql
@@ -1,4 +1,5 @@
-/* In this project, you’re going to make your own table with some small set of “famous people”, then make more tables about things they do and join those to create nice human readable lists. */
+/* In this project, you’re going to make your own table with some small set of “famous people”, 
+then make more tables about things they do and join those to create nice human readable lists. */
 
 
 
